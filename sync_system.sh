8a239d4bd439a1dbae9b91ea506b40d7f9315408
josemarialanda <josemaria.landa@gmail.com>
2021-10-10 16:10:28 -0500

    first sync

--- sync_system.sh
+++ sync_system.sh
@@ -1,7 +1,11 @@
 #!/run/current-system/sw/bin/bash
 
+github_username= "josemarialanda"
+github_token="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"
+
 if [ -d .git ]; then
   echo "Syncing system configuration files to git repo..."
+  echo "Access token: $github_token"
 else
   echo "Setting up local git repository"
   git init
@@ -18,8 +22,9 @@ cp ../.config/nixpkgs/home.nix ./
 # retrieve latest system configuration
 cp /etc/nixos/configuration.nix ./
 
-git add .
-
+git add -A .
 git commit -m "$commitMsg"
 git branch -M main
-git push -u origin main
+# git push -u origin main
+git push -u https://$github_username:$github_token@nixos-config.git
+
